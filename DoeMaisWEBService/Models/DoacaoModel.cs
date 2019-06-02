7b41f1e95bc07ce409ff10a063ce4c302091e6d6
David Lima <david.lima50@etec.sp.gov.br>
2019-06-02 15:26:14 -0300

    models finished

--- DoeMaisWEBService/Models/DoacaoModel.cs
+++ DoeMaisWEBService/Models/DoacaoModel.cs
@@ -9,5 +9,12 @@ namespace DoeMaisWEBService.Models
     {
         private String instituicao;
         private String dataDoada;
+        private String dataParaDoar;
+        private String cod;
+
+        public string Instituicao { get => instituicao; set => instituicao = value; }
+        public string DataDoada { get => dataDoada ?? String.Empty; set => dataDoada = value ?? String.Empty; }
+        public string DataParaDoar { get => dataParaDoar; set => dataParaDoar = value; }
+        public string Cod { get => cod; set => cod = value; }
     }
 }
